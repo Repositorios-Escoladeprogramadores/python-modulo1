c87013fcca77abbe8b9418320efa8d94aba70c4a
Repositorios-Escoladeprogramadores <eprog.git@gmail.com>
2025-05-06 19:51:52 -0300

    Create exer_decisoes.py

---
+++ exercicios/decisoes/exer_decisoes.py
@@ -0,0 +1,30 @@
+# ==============================
+# Exercicio 1
+'''
+PROBLEMA: Todos os funcionários irão receber além do seu Salário Base uma
+Gratificação que irá corresponder a 10% do seu Salário Base.
+Somados Salário Base + Gratificação teremos seu Salário Bruto.
+Será descontado o IR (Imposto de Renda) de acordo com o Salário Bruto na regra:
+Até 1.200 = 15%
+Acima de 1.200 = 20%
+Entradas: Nome do funcioanario, SalarioBase
+Calculos: Gratificacao, SalarioBruto e IR
+Saidas: SALARIO BASE, GRATIFICACAO, IR e SALARIO BRUTO
+'''
+
+
+# ==============================
+# Exercicio 2
+'''
+PROBLEMA: A empresa XPTO com sede em SP trabalha com aluguel de carros para
+longas distâncias. Você deverá informar o Nome do Passageiro e a Cidade de
+Destino (considerar somente Vitoria, Muriae e Niteroi).
+Calcular o custo do aluguel (viagem) considerando o valor fixo de R$ 2,20 por cada
+KM da distância entre sede até destino.
+Considerar as distancias aproximadas entre sede e destino:
+Muriae 628, Niteroi 424, Vitoria 949
+Ao final imprimir: PASSAGEIRO, CIDADE, DISTANCIA e VALOR DA VIAGEM.
+Exceção: Se for recebido qualquer cidade que não da lista acima , será
+impresso somente nome do cliente e mensagem “Cidade não atendida pela
+empresa”. '''
+
