07a5e0896672fe9478a671cde3fcff905639f6e6
Repositorios-Escoladeprogramadores <eprog.git@gmail.com>
2025-05-06 12:38:50 -0300

    Create exer_variaveis.py

---
+++ exercicios/variaveis/exer_variaveis.py
@@ -0,0 +1,7 @@
+# Iremos desenvolver uma aplicação que receba o nome e duas notas de um aluno (entre 0.00 e 10.00) . Calcular e imprimir sua media.
+
+# Esse aqui eu faço por você..
+
+nome = ""
+
+# Agora é contigo
