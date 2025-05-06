39cf924c66dd161bb5df6cc3dc54354277b445e6
Repositorios-Escoladeprogramadores <eprog.git@gmail.com>
2025-05-06 12:29:22 -0300

    Create exer_operadores.py

---
+++ exercicios/operadores/exer_operadores.py
@@ -0,0 +1,8 @@
+# Considerando dois numeros (ex: 04 e 07) como valores realizar as 
+# operacoes : Soma, Subtração , Multiplicação, Divisão, Resto e Potencia
+
+# Esse aqui eu faço por você..
+
+print("Soma ", 4 + 7)
+
+# Agora é contigo
