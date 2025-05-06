1466804f8e83d615244d87e75d816352b8064522
Repositorios-Escoladeprogramadores <eprog.git@gmail.com>
2025-05-06 20:02:16 -0300

    Create exer_repeticoes_enquanto.py

---
+++ exercicios/repeticoes_euquanto/exer_repeticoes_enquanto.py
@@ -0,0 +1,8 @@
+# ==============================
+# Exercicio 
+'''
+Elabore um programa que leia um número N até que N = 0.
+Dentro do loop, se N for par acumular o valor recebido em acumulador par, se for ímpar acumular em
+acumulador ímpar.
+Ao final mostrar os valores dos dois acumuladores. (dica: usar % para rest)
+'''
