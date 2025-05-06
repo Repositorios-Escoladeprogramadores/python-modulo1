e129ea8dfa2cbba58001d6ffe7ca8db32a9c96f5
Repositorios-Escoladeprogramadores <eprog.git@gmail.com>
2025-05-06 20:33:38 -0300

    Create exer_repeticoes_for.py

---
+++ exercicios/repeticoes_for/exer_repeticoes_for.py
@@ -0,0 +1,9 @@
+# ==============================
+# Exercicio 1
+'''
+Problema: Iremos desenvolver uma solução para mostrar qual o valor final de um
+investimento financeiro.
+Receber um valor como Capital de entrada, número de meses aplicados e taxa de
+rentabilidade ao mês.
+Ao final imprimir: Capital de entrada, Meses, Taxa e Valor Capitalizado (final).
+'''
