e851f659e58706fb17bb39a0c0ff30f1b2d5c5f1
Repositorios-Escoladeprogramadores <eprog.git@gmail.com>
2025-05-06 20:43:44 -0300

    Create repeticoes_true_string.py

---
+++ exercicios/repeticoes_true_string/repeticoes_true_string.py
@@ -0,0 +1,8 @@
+# ==============================
+# Exercicio 
+'''
+Problema: Desenvolver uma solução para simular um caixa de supermercado.
+Iremos receber os produtos e seus valores dentro de um loop.
+Devemos implementar uma solução de saída do Loop.
+Ao final, imprimir somente o Total da Compra (soma dos produtos).
+'''
