51b98c432fb62a07eab89d62b821d290cb690915
Repositorios-Escoladeprogramadores <eprog.git@gmail.com>
2025-05-06 19:32:29 -0300

    Update exer_operadores.py

--- exercicios/operadores/exer_operadores.py
+++ exercicios/operadores/exer_operadores.py
@@ -1,3 +1,4 @@
+# Exercicio 1
 # Considerando dois numeros (ex: 04 e 07) como valores realizar as 
 # operacoes : Soma, Subtração , Multiplicação, Divisão, Resto e Potencia (Soma dos dois elevado a potencia de 3)
 
@@ -5,4 +6,19 @@
 
 print("Soma ", 4 + 7)
 
+# Agora é contigo
+
+
+
+
+
+# ===========================
+# Exercicio 2
+# Desenvolva uma solução que receba 02 (dois) números cada qual em
+# uma variável. O objetivo é trocar os valores entre as variáveis , sendo que a primeira
+# terá o valor da segunda e vice-versa.
+# Troque os valores entre as variaveis ficando B=15 e A=12
+
+A=15
+B=12
 # Agora é contigo
