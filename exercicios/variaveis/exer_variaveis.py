97579e49519b5c4157d736e708e4f8cc9a85f049
Repositorios-Escoladeprogramadores <eprog.git@gmail.com>
2025-05-06 19:19:27 -0300

    Update exer_variaveis.py

--- exercicios/variaveis/exer_variaveis.py
+++ exercicios/variaveis/exer_variaveis.py
@@ -1,7 +1,23 @@
+#==========================================================================
+#  Proposta de Exercicio 1
 # Iremos desenvolver uma aplicação que receba o nome e duas notas de um aluno (entre 0.00 e 10.00) . Calcular e imprimir sua media.
 
-# Esse aqui eu faço por você..
+# Deixa eu te ajudar ..
 
 nome = ""
+# Agora você continua
 
-# Agora é contigo
+
+#==========================================================================
+#  Proposta de Exercicio 2
+#  PROBLEMA: DADOS BASE E ALTURA DE UM TRIANGULO, CALCULAR E IMPRIMIR SUA ÁREA
+
+# Deixa eu te ajudar ..
+
+base   = 0.00
+altura = 0.00
+area   = 0.00 
+
+# Agora você continua
+
+
