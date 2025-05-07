2d3f50e5d466fd33c36f4236b17c8a24f134af61
Repositorios-Escoladeprogramadores <eprog.git@gmail.com>
2025-05-07 11:17:41 -0300

    Fontes

---
+++ exercicios/modulos/exer_modulos.py
@@ -0,0 +1,16 @@
+# ==============================
+# Exercicio 
+'''
+Serão duas etapas:
+Iremos gerar um numero qualquer entre 1 e 10 atraves da funcao RANDOM.
+
+Etapa 1:
+Iremos receber o usuario um numero qualquer entre 0 e 10 e comparar com o numero gerado.
+Se bater iremos mostrar mensagem "Acertou" senão "Errou"
+Nessa etapa o usuario tem apenas uma chance
+
+Etapa 2:
+Dentro de um loop TRUE iremos deixar o usuario chutar o numero.
+ Enquanto ele não acertar o loop não acaba.
+ Ao final iremos dizer de quantas tentativas ele precisou até acertar
+ '''
