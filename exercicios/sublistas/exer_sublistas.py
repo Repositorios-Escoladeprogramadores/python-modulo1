9934e8d9da67648813fd7463fda6c393f0ccf06a
Repositorios-Escoladeprogramadores <eprog.git@gmail.com>
2025-05-07 09:48:28 -0300

    Create exer_sublistas.py

---
+++ exercicios/sublistas/exer_sublistas.py
@@ -0,0 +1,6 @@
+# ==============================
+# Exercicio 
+'''
+Dado a lista abaixo, faça a separação em sublistas para ano  e presidente .
+Iremos receber via input um determinado ano. Devemos validar se existe , procurar (e mostrar) o presidente daquele ano.
+'''
