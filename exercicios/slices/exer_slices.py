2b3474805c9854eb6870c879bc9b6ac029dc5d5a
Repositorios-Escoladeprogramadores <eprog.git@gmail.com>
2025-05-07 10:39:55 -0300

    csd

---
+++ exercicios/slices/exer_slices.py
@@ -0,0 +1,8 @@
+# ==============================
+# Exercicio 
+'''
+Dado a lista abaixo, extrair uma sublista com os nomes dos itens e outra com seus codigos:
+1 - Extrair a partir do item 1 somente os codigos com valor impar e em outra sublista os nomes dos items relativos aos codigos
+2 - Mudar a ordem para descendente (maior para o menor) 
+3 - Imprimir as sublistas com codigos e itens  (impar e descendente)
+'''
---
+++ exercicios/split/exer_split.py
@@ -0,0 +1,13 @@
+# ==============================
+# Exercicio 
+'''
+Serão duas propostas usando a variavel codigoHTML onde temos um trecho de uma pagina na linguagem html.
+1 - Usando o SPLIT devolver a quantidade de ocorrencias da TAG <BR> encontrada na variavel
+2 - Aproveitando a 1a resolução, procurar por ocorrencias da TAG <SPAN> e tambem seu fechamento </SPAN>. 
+Se a quantidade TOTAL das duas ocorrencias for impar significa que uma está ausente da outra, então imprimir mensagem indicando analise
+caso contrario imprimir que está ok com a TAG.
+'''
+
+codigoHTML = "<HTML> <TITLE> <BODY> <H4> PAGINA TESTE  </H4> <BR> <SPAN> OBJETIVO: VITRINE DE PRODUTOS </SPAN> <BR> " \
+"<SPAN> CLIENTE: ESCOLADEPROGRAMADORES </SPAN> <BR>  DESENVOLVIDO POR: ISP </SPAN> <BR> </BODY> </HTML>"
+
