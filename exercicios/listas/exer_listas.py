bb8bc7f0f6a5eb85a2a3bce5aa158e8e9b990802
Repositorios-Escoladeprogramadores <eprog.git@gmail.com>
2025-05-06 21:20:18 -0300

    Create exer_listas.py

---
+++ exercicios/listas/exer_listas.py
@@ -0,0 +1,12 @@
+# ==============================
+# Exercicio 1
+'''
+Problema: Criar uma solução que receba 05 numeros (em uma lista com itens inteiros) , calcule a media destes numeros e imprima a média. 
+Imprima ao final QUAIS dos numeros informados é maior ou igual ao valor da media (somente os maiores)
+'''
+
+# ==============================
+# Exercicio 2
+'''
+Problema: Criar uma solução que receba 06 numeros (em uma lista com valores inteiros) .
+Mostre ao final qual foi o MAIOR e o MENOR valor recebido.
